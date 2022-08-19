630a7fc4e824db320b90406592117fa2dd4ac3e5
mabo1975 <boca.marcelo@gmail.com>
2022-08-19 15:17:10 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cusd%20coin%2C%20bnb%2Cxrp&vs_currencies=usd&include_24hr_change=true')
+    .then(res => res.json())
+    .then(json => {
+        const container = document.querySelector(".container");
+        const coins = Object.getOwnPropertyNames(json);
+
+        for (let coin of coins) {
+
+            const coinInfo = json[`${coin}`];
+            const price = coinInfo.usd;
+            const change = coinInfo.usd_24h_change.toFixed(5); // 0.12345
+
+            container.innerHTML += `
+                <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+                    <div class="coin-logo">
+                        <img src="img/${coin}.png"></div>
+                    <div class="coin-name">
+                        <h3>${coin}</h3>
+                        <span>/USD</span>
+                    </div>
+                    <div class="coin-price">
+                        <span class = "price">$${price}</span>
+                        <span class = "change">${change}</span>
+                    </div>
+                </div>
+                 
+                `;
+
+        }
+
+    });
+
